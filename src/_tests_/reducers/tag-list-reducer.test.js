184f1bc477eb7edbc2bd224024c8898759a98a72
Tiffany Siu <tsiu88@gmail.com>
2020-05-18 16:59:25 -0700

    start adding tests for redux

--- src/_tests_/reducers/tag-list-reducer.test.js
+++ src/_tests_/reducers/tag-list-reducer.test.js
@@ -1,7 +1,52 @@
 import tagListReducer from '../../reducers/tag-list-reducer';
+import {v4} from 'uuid';
 
 describe('tagListReducer', () => {
+
+  let action;
+  const firstId = v4();
+  const tagData = {
+    id: firstId,
+    tagStatus: true,
+    nickName: "nick one",
+    placeName: "place name",
+    description: "tester place desc",
+    address: "123 Street",
+    coordinates: "xxx,yyy",
+    personalNote: "testing testing",
+    dateCreated: Date.now()
+  };
   test('Should return default state if no action type passed into reducer', () => {
     expect(tagListReducer({}, {type: null})).toEqual({});
   });
+
+  test('Should add new tag to masterTagList', () => {
+    const { id, tagStatus, nickName, placeName, description, address, coordinates, personalNote, dateCreated} = tagData;
+    action = {
+      type: 'ADD_OR_UPDATE_TAG',
+      id: id,
+      tagStatus: tagStatus,
+      nickName: nickName,
+      placeName: placeName,
+      description: description,
+      address: address,
+      coordinates: coordinates,
+      personalNote: personalNote,
+      dateCreated: dateCreated
+    };
+    expect(tagListReducer({}, action)).toEqual({
+      [id]: {
+        id: id,
+        tagStatus: tagStatus,
+        nickName: nickName,
+        placeName: placeName,
+        description: description,
+        address: address,
+        coordinates: coordinates,
+        personalNote: personalNote,
+        dateCreated: dateCreated
+      }
+    })
+  });
+
 });
--- src/reducers/tag-list-reducer.js
+++ src/reducers/tag-list-reducer.js
@@ -1,3 +1,21 @@
 export default (state = {}, action) => {
-  return state;
+  const { id, tagStatus, nickName, placeName, description, address, coordinates, personalNote, dateCreated} = action;
+  switch (action.type){
+    case 'ADD_OR_UPDATE_TAG':
+      return Object.assign({}, state, {
+        [id]: {
+          id: id,
+          tagStatus: tagStatus,
+          nickName: nickName,
+          placeName: placeName,
+          description: description,
+          address: address,
+          coordinates: coordinates,
+          personalNote: personalNote,
+          dateCreated: dateCreated
+        }
+      });
+    default:
+      return state;
+  }
 };
