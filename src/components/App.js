751feaf01d6c9ab145c24c207c39b39962109506
Tiffany Siu <tsiu88@gmail.com>
2020-05-19 16:17:26 -0700

    Add routing, header links

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
-import ScreenControl from './ScreenControl'
+import ScreenControl from './ScreenControl';
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
-      {/* <Router> */}
+      <Router>
         <Header />
-        <ScreenControl />
-      {/* </Router> */}
+        <Switch>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/">
+           <ScreenControl />
+          </Route>
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return(
-    <h1>Been There, Done That</h1>
+    <React.Fragment>
+      <h1>Been There, Done That</h1>
+      <div>
+        <h4>
+        <Link to="/">Home</Link>
+        {`\t`}|{`\t`}
+        <Link to="/signin">Sign In</Link>
+        {`\t`}|{`\t`}
+        <Link to="/register">Register</Link>
+        </h4>
+      </div>
+    </React.Fragment>
   );
 }
 
